62058cbba16bc23a7eb93182d29c4e5e0ec5d8f3
Tal Taub <taltaub22@gmail.com>
2018-02-17 11:55:38 +0200

    Conversion no longer in place.

--- Convert.js
+++ Convert.js
@@ -16,19 +16,21 @@ function ConvertPath(path) {
             if (stats.isDirectory()) {
                 try {
                     convertFolder(path);
+                    alert("Folder Successfully converted!")
                 } catch (err) {
                     alert("Error while converting files!");
                     console.error(err);
                 }
-                alert("Folder Successfully converted!")
+
             } else {
                 try {
                     convertFile(path);
+                    alert("File Successfully converted!")
                 } catch (err) {
-                    alert("Error while converting file!");
                     console.error(err);
+                    alert("Error while converting file! \n" + err);
                 }
-                alert("File Successfully converted!")
+
             }
 
 
